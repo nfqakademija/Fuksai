b356bb5d438fbf95b615297522785195ba3b757d
shalifar <shalifar2010@gmail.com>
2016-12-02 23:00:55 +0200

    subscriber entity subscribe controller subscriber addition to database form

---
+++ src/AppBundle/Controller/SubscribeController.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shalifar
+ * Date: 16.12.2
+ * Time: 21.45
+ */
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Subscriber;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+
+class SubscribeController extends Controller
+{
+    /**
+     * @Route("/subscribe")
+     *
+     * @param Request
+     * @return \Symfony\Component\HttpFoundation\Request
+     */
+    public function showForm(Request $request)
+    {
+        $subscriber = new Subscriber();
+
+        $form = $this->createFormBuilder($subscriber)
+            ->add('email', TextType::class)
+            ->add('submit', SubmitType::class, ['label'=>'Subscribe'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+
+            $subscriber = $form->getData();
+
+            //email check
+            if(!is_null($em->getRepository('AppBundle:Subscriber')->findBy(['email' => $subscriber->getEmail()]))){
+                //such email exists, notify user
+            }
+
+            //keyName check
+            do{
+                $subscriber->setKeyName(rand(1000, 9999));
+            }
+            while(!is_null($em->getRepository('AppBundle:Subscriber')->findBy([
+                'keyName' => $subscriber->getKeyName()
+            ])));
+
+            $em->persist($subscriber);
+            $em->flush();
+        }
+
+        return $this->render('subscribe/subscribe_form.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/AppBundle/Entity/Subscriber.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Subscriber
+ *
+ * @ORM\Table(name="subscriber")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SubscriberRepository")
+ */
+class Subscriber
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="key_name", type="integer", unique=true)
+     */
+    private $keyName;
+
+
+    /**
+     * @return int
+     */
+    public function getKeyName()
+    {
+        return $this->keyName;
+    }
+
+    /**
+     * @param int $keyName
+     */
+    public function setKeyName($keyName)
+    {
+        $this->keyName = $keyName;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Subscriber
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
+
