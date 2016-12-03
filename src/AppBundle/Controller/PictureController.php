4b4462b3797d8961e3974169b4a004bf92fde765
Logo252 <arnasgrigaliunas8@gmail.com>
2016-12-03 15:01:26 +0200

    modify astronomy picture layout and controller

--- src/AppBundle/Controller/PictureController.php
+++ src/AppBundle/Controller/PictureController.php
@@ -8,15 +8,31 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class PictureController extends Controller
 {
     /**
-     * @Route("/astronomy-picture-of-the-day", name="astronomy_picture")
+     * @Route("/astronomy-picture-of-the-day/{picture}", name="astronomy_picture")
+     * @param $picture
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function picturesAction()
+    public function pictureAction($picture)
     {
         $em = $this->getDoctrine()->getManager();
         $pictures = $em->getRepository('AppBundle:Picture')->findAll();
 
         return $this->render('picture/astronomy_picture.html.twig', [
+            'pictures' => $pictures,
+            'picture' => $picture,
+        ]);
+    }
+
+    /**
+     * @Route("/astronomy-pictures-of-the-day", name="astronomy_pictures")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function allPicturesAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $pictures = $em->getRepository('AppBundle:Picture')->findAll();
+
+        return $this->render('picture/all_astronomy_pictures.html.twig', [
             'pictures' => $pictures,
         ]);
     }
