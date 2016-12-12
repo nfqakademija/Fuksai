846d6a73a1e809afbcff376c9cd30279f8589eb5
artik1327 <artur.litvinavicius@gmail.com>
2016-12-12 13:28:57 +0200

    deleted console log commands

--- web/js/apod-responsive.js
+++ web/js/apod-responsive.js
@@ -3,8 +3,6 @@ $(window).load(function() {
     $('.image-container').each(function (i , obj){
 
         var boxheight = $(this).children('.picture-image').innerHeight();
-        console.log(boxheight);
-        console.log(this);
         $(this).children('.picture-text').outerHeight(boxheight);
     });
 });
